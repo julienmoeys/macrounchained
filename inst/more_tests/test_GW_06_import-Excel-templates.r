78e77a1c787b0ad9a636d00403cf354977293dbe
Julien Moeys <julienmoeys@yahoo.fr>
2019-01-23 16:12:35 +0100

    Create test_GW_06_import-Excel-templates.r

---
+++ inst/more_tests/test_GW_06_import-Excel-templates.r
@@ -0,0 +1,66 @@
+
+rm(list=ls(all=TRUE)) 
+
+prj_name  <- "macrounchained" 
+pkg_name  <- "macrounchained"
+test_name <- "test_GW_06_import-Excel-templates"
+
+if( as.logical( as.integer( Sys.getenv( "macrounchained_developer", unset = "0" ) ) ) ){
+    rPackagesDir <- Sys.getenv( "rPackagesDir", unset = NA_character_ )
+    
+    if( !is.na( rPackagesDir ) ){ 
+         output_dir <- file.path( rPackagesDir, prj_name, 
+            pkg_name, "inst", "more_tests", "results" ) 
+    }else{
+        output_dir <- getwd()
+    }   
+}else{
+    output_dir <- getwd()
+}   
+
+library( "macrounchained" )
+# library( "readxl" ) 
+
+xlsx <- system.file( "xlsx", "macrounchainedFocusGW-with-met.xlsx", 
+    package = "macrounchained" ) 
+
+param <- readxl::read_excel( 
+    path = xlsx, 
+    sheet = "s" )
+
+#   Inspect
+as.data.frame( param ) 
+
+parfile <- system.file( "par-files", 
+    "chat_winCer_GW-X_900gHa_d182.par", package = "rmacrolite" )
+
+res <- macrounchainedFocusGW( 
+    s         = param, 
+    parfile   = parfile, 
+    overwrite = TRUE, 
+    run       = FALSE ) # Dry run 
+
+
+
+
+xlsx2 <- system.file( "xlsx", 
+    "macrounchainedFocusGW-multiple-applications.xlsx", 
+    package = "macrounchained" ) 
+
+param2 <- readxl::read_excel( 
+    path = xlsx2, 
+    sheet = "s" )
+
+#   Inspect
+as.data.frame( param2 ) 
+
+parfile2 <- system.file( "par-files", 
+    "chat_winCer_GW-X_900gHa_d182_850gHa_d189.par", 
+    package = "rmacrolite" )
+
+res2 <- macrounchainedFocusGW( 
+    s         = param2, 
+    parfile   = parfile2, 
+    overwrite = TRUE, 
+    run       = FALSE ) # Dry run 
+
