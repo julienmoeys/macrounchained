44ea6ff2156c7c15634e3e99cbbba647cd8175d1
Julien Moeys <julienmoeys@yahoo.fr>
2019-02-15 13:15:17 +0100

    First ready version of the package vignette

--- package_vignette_build.r
+++ package_vignette_build.r
@@ -7,8 +7,10 @@ prjName <- "macrounchained"
 #   Package name (in the project)
 pkgName <- "macrounchained"
 
+vignetteName <- "macrounchainedfocusgw-manual.%s"
+
 setwd( file.path( Sys.getenv( x = "rPackagesDir" ), prjName, 
-    pkgName, "vignettes" ) )
+    pkgName ) )
 
 #   Source package
 # knitr::knit2html( input = "macrounchainedfocusgw-manual.Rmd", 
@@ -18,9 +20,14 @@ setwd( file.path( Sys.getenv( x = "rPackagesDir" ), prjName,
 
 # rmarkdown::render( input = "macrounchainedfocusgw-manual.Rmd", envir = e <- new.env() )
 
+knitr::knit( 
+    input  = file.path( "vignettes", sprintf( vignetteName, "Rmd" ) ), 
+    output = file.path( "vignettes", sprintf( vignetteName, "md" ) ), 
+    envir  = e1 <- new.env() )
 
-
-knitr::knit( input = "macrounchainedfocusgw-manual.Rmd", envir = e <- new.env() )
-
-rmarkdown::render( input = "macrounchainedfocusgw-manual.md", envir = e <- new.env() )
+rmarkdown::render( 
+    input       = file.path( "vignettes", sprintf( vignetteName, "md" ) ), 
+    output_file = sprintf( vignetteName, "html" ), 
+    output_dir  = file.path( getwd(), "inst", "doc" ), 
+    envir       = e2 <- new.env() )
 
