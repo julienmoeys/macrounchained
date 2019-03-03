60f6285ed8feddb7d9844a85b51933215b6735bc
Julien Moeys <julienmoeys@yahoo.fr>
2019-03-03 14:47:09 +0100

    Prepare new build

--- package_build.r
+++ package_build.r
@@ -37,7 +37,7 @@ pdu_detach( pkgName = pkgName )
 pdu_pkgDescription( 
     pkgName     = pkgName, 
     pkgDir      = pkgDir, 
-    pkgVersion  = "0.9.2", 
+    pkgVersion  = "0.9.3", 
     pkgDepends  = NULL, 
     pkgImports  = c( "rmacrolite", "macroutils2", "codeinfo", 
         "utils", "stats" ), 
