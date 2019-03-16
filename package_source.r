3429e10eb7a1012a4c430e74fee61fba403c3791
Julien Moeys <julienmoeys@yahoo.fr>
2019-03-16 16:03:30 +0100

    Improve the sourcing of the package

--- package_source.r
+++ package_source.r
@@ -10,5 +10,9 @@ pkgName <- "macrounchained"
 setwd( file.path( Sys.getenv( x = "rPackagesDir" ), prjName, 
     pkgName ) )
 
+library( "codeinfo" )
+library( "macroutils2" )
+library( "rmacrolite" )
+
 #   Source package
 source( sprintf( "R/%s.r", pkgName ) )
