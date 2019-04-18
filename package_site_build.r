a565d20b681334da26f757a7369766d98a30d78f
Julien Moeys <julienmoeys@yahoo.fr>
2019-04-18 17:20:41 +0200

    Build package website

--- package_site_build.r
+++ package_site_build.r
@@ -7,8 +7,6 @@ prjName <- "macrounchained"
 #   Package name (in the project)
 pkgName <- "macrounchained"
 
-vignetteName <- "macrounchainedfocusgw-manual.%s"
-
 setwd( file.path( Sys.getenv( x = "rPackagesDir" ), prjName, 
     pkgName ) )
 
