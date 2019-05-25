e5467b6a0ea0cf0e7e6f94ae398952f903d296e0
Julien Moeys <julienmoeys@yahoo.fr>
2019-05-25 13:57:00 +0200

    Correct specification of the transformation pathways

--- inst/more_tests/test_GW_10_multiple-paths.r
+++ inst/more_tests/test_GW_10_multiple-paths.r
@@ -11,6 +11,8 @@
 
 
 
+#   options( warn = 2, error = recover )
+
 rm(list=ls(all=TRUE)) 
 
 prj_name  <- "macrounchained" 
@@ -35,21 +37,21 @@ library( "macrounchained" )
 param <- data.frame( 
     "soil"           = "chateaudun", 
     "crop"           = "cereals, spring", 
-    "id"             = c(     1L,       2L,        3L,            4L,    5L,       6L ), 
-    "name"           = c( "GW-M", "Met-M0",  "Met-M1",      "Met-M2", "GW-N", "GW-N1" ), 
-    "kfoc"           = c(     12,       10,       114,            71,    46,       89 ), 
-    "nf"             = c(      1,      0.8,       0.9,           0.9,   1.0,      0.8 ), 
-    "dt50"           = c(     23,       26,        19,           128,     5,      207 ), 
+    "id"             = c(     1L,       2L,        3L,            4L,    5L,        6L ), 
+    "name"           = c( "GW-M", "Met-M0",  "Met-M1",      "Met-M2", "GW-N", "Met-N1" ), 
+    "kfoc"           = c(     12,       10,       114,            71,    46,        89 ), 
+    "nf"             = c(      1,      0.8,       0.9,           0.9,   1.0,       0.8 ), 
+    "dt50"           = c(     23,       26,        19,           128,     5,       207 ), 
     "dt50_ref_temp"  = 20, 
     "dt50_pf"        = 2, 
     "exp_temp_resp"  = 0.0948, 
     "exp_moist_resp" = 0.49, 
     "crop_upt_f"     = c(    0.5,      0.0,       0.0,           0.0,   0.0,      0.0 ), 
     "diff_coef"      = 5E-10, 
-    "parent_id"      = c(     NA,      "1",     "1|2",       "1|3|5",     NA,     "5" ), 
-    "g_per_mol"      = c(    381,      367,       183,           140,    395,     154 ), 
-    "transf_f"       = c(     NA,    "0.2", "0.2|1.0", "0.2|1.0|1.0",     NA,   "1.0" ), # mol met formed / mol parent degraded
-    "g_as_per_ha"    = c(     3,         0,         0,             0,      6,       0 ), 
+    "parent_id"      = c(     NA,      "1",     "1|2",       "1|3|6",     NA,      "5" ), 
+    "g_per_mol"      = c(    381,      367,       183,           140,    395,      154 ), 
+    "transf_f"       = c(     NA,    "0.2", "0.2|1.0", "0.2|1.0|1.0",     NA,    "0.8" ), # mol met formed / mol parent degraded
+    "g_as_per_ha"    = c(     3,         0,         0,             0,      6,        0 ), 
     "app_j_day"      = 70L, # Day after emergence
     stringsAsFactors = FALSE 
 )   
